90de511ca32ea123ec992e8ec4d7537a8e49ad1a
Alessandro <alessandroassirelli98@gmail.com>
2024-04-29 14:30:02 +0200

    implemented episode cuts

--- omniisaacgymenvs/scripts/skrl/diana_tekken_PPOFD.py
+++ omniisaacgymenvs/scripts/skrl/diana_tekken_PPOFD.py
@@ -116,7 +116,7 @@ cfg["experiment"]["checkpoint_interval"] = 4000
 cfg["experiment"]["directory"] = "runs/torch/DianaTekken"
 cfg["experiment"]["wandb"] = True
 cfg["experiment"]["wandb_kwargs"] = {"tags" : ["PPOFD + BC"],
-                                     "project": "simplified model sparse rew no cut"}
+                                     "project": "simplified model sparse rew cut"}
 
 ignore_args = ["headless", "task", "num_envs"] # These shouldn't be handled by this fcn
 algo_config = parse_arguments(ignore_args)
