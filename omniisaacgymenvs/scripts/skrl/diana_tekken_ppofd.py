979e3f3b2a75e20bacda97b2a72cd4e355dff313
Alessandro <alessandroassirelli98@gmail.com>
2024-05-21 11:30:12 +0200

    training with 12 dof

--- omniisaacgymenvs/scripts/skrl/diana_tekken_ppofd.py
+++ omniisaacgymenvs/scripts/skrl/diana_tekken_ppofd.py
@@ -146,14 +146,14 @@ cfg["learning_epochs"] = 8
 cfg["mini_batches"] = 4  # 16 * 8192 / 32768
 cfg["discount_factor"] = 0.99
 cfg["lambda"] = 0.95
-cfg["learning_rate"] = 5e-4
+cfg["learning_rate"] = 1e-3
 cfg["random_timesteps"] = 0
 cfg["learning_starts"] = 0
 cfg["grad_norm_clip"] = 1.0
-cfg["ratio_clip"] = 0.2
+cfg["ratio_clip"] = 0.1
 cfg["value_clip"] = 0.2
 cfg["clip_predicted_values"] = True
-cfg["entropy_loss_scale"] = 0.0
+cfg["entropy_loss_scale"] = 0.001
 cfg["value_loss_scale"] = 2.0
 cfg["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
 
@@ -172,7 +172,7 @@ cfg["experiment"]["checkpoint_interval"] = 200
 cfg["experiment"]["directory"] = "runs/torch/DianaTekken"
 cfg["experiment"]["wandb"] = True
 cfg["experiment"]["wandb_kwargs"] = {"tags" : ["PPOFD "],
-                                     "project": "pick up trial"}
+                                     "project": "pick up trial 12 DOF"}
 
 ignore_args = ["headless", "task", "num_envs"] # These shouldn't be handled by this fcn
 algo_config = parse_arguments(ignore_args)
