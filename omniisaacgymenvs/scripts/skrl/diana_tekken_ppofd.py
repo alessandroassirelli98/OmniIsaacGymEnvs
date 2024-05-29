b6ea29f893337af0c465f5d94fe8316c85a764f4
Alessandro Assirelli <alessandroassirelli98@gmail.com>
2024-05-29 15:46:18 +0200

    combinatorial auto runs

--- omniisaacgymenvs/scripts/skrl/diana_tekken_ppofd.py
+++ omniisaacgymenvs/scripts/skrl/diana_tekken_ppofd.py
@@ -146,14 +146,14 @@ cfg["learning_epochs"] = 8
 cfg["mini_batches"] = 4  # 16 * 8192 / 32768
 cfg["discount_factor"] = 0.99
 cfg["lambda"] = 0.95
-cfg["learning_rate"] = 5e-4
+cfg["learning_rate"] = 1e-3
 cfg["random_timesteps"] = 0
 cfg["learning_starts"] = 0
 cfg["grad_norm_clip"] = 1.0
 cfg["ratio_clip"] = 0.2
 cfg["value_clip"] = 0.2
 cfg["clip_predicted_values"] = True
-cfg["entropy_loss_scale"] = 0.001
+cfg["entropy_loss_scale"] = 0.0001
 cfg["value_loss_scale"] = 2.0
 cfg["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
 
