2de32090d9e7ee8ae680b60f73bf833ffd3e39a4
Alessandro Assirelli <alessandroassirelli98@gmail.com>
2024-06-03 16:40:18 +0200

    less run

--- omniisaacgymenvs/scripts/skrl/diana_tekken_ppofd.py
+++ omniisaacgymenvs/scripts/skrl/diana_tekken_ppofd.py
@@ -232,7 +232,7 @@ agent = PPOFD(models=models,
 
 
 # configure and instantiate the RL trainer
-cfg_trainer = {"timesteps": 120000}
+cfg_trainer = {"timesteps": 80000}
 trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
 # demonstrations injection
