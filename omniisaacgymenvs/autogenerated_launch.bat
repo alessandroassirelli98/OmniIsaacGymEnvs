59c8b03f86bf3eefa9ccefea7ae7e61b6a01add6
alessandro assirelli <alessandroassirelli98@gmail.com>
2024-06-03 17:27:47 +0200

    added auto run files for windows

---
+++ omniisaacgymenvs/autogenerated_launch.bat
@@ -0,0 +1,144 @@
+
+
+echo '------ Running 1 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.05 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 2 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.05 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 3 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.05 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 4 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.05 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 5 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.1 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 6 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.1 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 7 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.1 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 8 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.1 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 9 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.2 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 10 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.2 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 11 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.2 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 12 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0 +ratio_clip=0.2 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 13 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.05 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 14 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.05 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 15 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.05 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 16 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.05 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 17 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.1 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 18 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.1 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 19 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.1 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 20 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.1 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 21 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.2 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 22 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.2 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 23 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.2 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 24 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.0001 +ratio_clip=0.2 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 25 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.05 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 26 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.05 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 27 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.05 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 28 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.05 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 29 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.1 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 30 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.1 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 31 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.1 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 32 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.1 +learning_rate=0.0005 +mini_batches=16
+
+
+echo '------ Running 33 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.2 +learning_rate=0.001 +mini_batches=4
+
+
+echo '------ Running 34 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.2 +learning_rate=0.001 +mini_batches=16
+
+
+echo '------ Running 35 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.2 +learning_rate=0.0005 +mini_batches=4
+
+
+echo '------ Running 36 run over 36 -------'
+C:/Users/OWS-User/AppData/Local/ov/pkg/isaac_sim-2023.1.1/python.bat scripts/skrl/diana_tekken_ppofd.py +entropy_loss_scale=0.001 +ratio_clip=0.2 +learning_rate=0.0005 +mini_batches=16
