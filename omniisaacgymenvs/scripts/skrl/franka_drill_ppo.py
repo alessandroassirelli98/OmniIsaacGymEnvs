9d57210ecf8425447f0da50f2ffd2f73a76ac1b5
Alessandro Assirelli <alessandro.assirelli@ar-lx0265.agile-robots.de>
2024-06-21 10:03:01 +0200

    running updated task

--- omniisaacgymenvs/scripts/skrl/franka_drill_ppo.py
+++ omniisaacgymenvs/scripts/skrl/franka_drill_ppo.py
@@ -166,7 +166,7 @@ cfg["experiment"]["checkpoint_interval"] = 200
 cfg["experiment"]["directory"] = "runs/torch/DianaTekken"
 cfg["experiment"]["wandb"] = True
 cfg["experiment"]["wandb_kwargs"] = {"tags" : ["PPO"],
-                                     "project": "franka_tekken 12 dof js"}
+                                     "project": "franka_tekken 12 dof js rev2"}
 
 ignore_args = ["headless", "task", "num_envs"] # These shouldn't be handled by this fcn
 algo_config = parse_arguments(ignore_args)
